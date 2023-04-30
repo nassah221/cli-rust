e41f8d22082f9920f1b7ce357efe647c024d9fc9
Muhammad Hassan <hassanmuhammad221@gmail.com>
2023-04-30 20:25:31 +0500

    Add even more tests

--- catr/tests/cli.rs
+++ catr/tests/cli.rs
@@ -80,3 +80,86 @@ fn bustle_n() -> TestResult {
 fn bustle_b() -> TestResult {
     run(&["-b", BUSTLE], "tests/expected/bustle.txt.b.out")
 }
+
+#[test]
+fn bustle_stdin() -> TestResult {
+    run_stdin(BUSTLE, &["-"], "tests/expected/bustle.txt.stdin.out")
+}
+
+#[test]
+fn bustle_stdin_n() -> TestResult {
+    run_stdin(
+        BUSTLE,
+        &["-n", "-"],
+        "tests/expected/bustle.txt.n.stdin.out",
+    )
+}
+
+#[test]
+fn bustle_stdin_b() -> TestResult {
+    run_stdin(
+        BUSTLE,
+        &["-b", "-"],
+        "tests/expected/bustle.txt.b.stdin.out",
+    )
+}
+
+#[test]
+fn empty() -> TestResult {
+    run(&[EMPTY], "tests/expected/empty.txt.out")
+}
+
+#[test]
+fn empty_n() -> TestResult {
+    run(&["-n", EMPTY], "tests/expected/empty.txt.n.out")
+}
+
+#[test]
+fn empty_b() -> TestResult {
+    run(&["-b", EMPTY], "tests/expected/empty.txt.b.out")
+}
+
+#[test]
+fn fox() -> TestResult {
+    run(&[FOX], "tests/expected/fox.txt.out")
+}
+
+#[test]
+fn fox_n() -> TestResult {
+    run(&["-n", FOX], "tests/expected/fox.txt.n.out")
+}
+
+#[test]
+fn fox_b() -> TestResult {
+    run(&["-b", FOX], "tests/expected/fox.txt.b.out")
+}
+
+#[test]
+fn spiders() -> TestResult {
+    run(&[SPIDER], "tests/expected/spider.txt.out")
+}
+
+#[test]
+fn spiders_n() -> TestResult {
+    run(&["-n", SPIDER], "tests/expected/spider.txt.n.out")
+}
+
+#[test]
+fn spiders_b() -> TestResult {
+    run(&["-b", SPIDER], "tests/expected/spider.txt.b.out")
+}
+
+#[test]
+fn all() -> TestResult {
+    run(&[SPIDER, FOX, BUSTLE], "tests/expected/all.out")
+}
+
+#[test]
+fn all_n() -> TestResult {
+    run(&[SPIDER, FOX, BUSTLE, "-n"], "tests/expected/all.n.out")
+}
+
+#[test]
+fn all_b() -> TestResult {
+    run(&[SPIDER, FOX, BUSTLE, "-b"], "tests/expected/all.b.out")
+}
