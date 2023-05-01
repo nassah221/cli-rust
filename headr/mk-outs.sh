645589adc014d9655232067e7ed9dc382a09dff6
Muhammad Hassan <hassanmuhammad221@gmail.com>
2023-05-01 13:04:17 +0500

    Fix test file script

--- headr/mk-outs.sh
+++ headr/mk-outs.sh
@@ -17,6 +17,16 @@ for FILE in $ALL; do
     BASENAME=$(basename $FILE)
 
     head $FILE > ${OUT_DIR}/${BASENAME}.out
-    head -n 2 $FILE > ${OUT_DIR}/${BASENAME}.n.out
+    head -n 2 $FILE > ${OUT_DIR}/${BASENAME}.n2.out
+    head -n 4 $FILE > ${OUT_DIR}/${BASENAME}.n4.out
+    head -c 1 $FILE > ${OUT_DIR}/${BASENAME}.c1.out
+    head -c 2 $FILE > ${OUT_DIR}/${BASENAME}.c2.out
+    head -c 4 $FILE > ${OUT_DIR}/${BASENAME}.c4.out
 done
 
+head $ALL > ${OUT_DIR}/all.txt.out
+head -n 2 $ALL > ${OUT_DIR}/all.txt.n2.out
+head -n 4 $ALL > ${OUT_DIR}/all.txt.n4.out
+head -c 1 $ALL > ${OUT_DIR}/all.txt.c1.out
+head -c 2 $ALL > ${OUT_DIR}/all.txt.c2.out
+head -c 4 $ALL > ${OUT_DIR}/all.txt.c4.out
